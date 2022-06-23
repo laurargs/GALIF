404efa21ffea060b64852182dc27ca45e45024cf
laurargs <103062837+laurargs@users.noreply.github.com>
2022-06-23 11:58:41 -0300

    Create test.dart

---
+++ test.dart
@@ -0,0 +1,29 @@
+appBar: AppBar(
+
+        toolbarHeight: 100,
+
+        backgroundColor: Color(0xFFF4DE9C),
+
+
+        title: const Text(
+                "GALIF",
+                style: TextStyle(color: Color(0xFF571f04)),
+       ),
+
+        leading: const Icon(
+                Icons.arrow_back_ios_rounded,
+                color: Color(0xFF571f04),
+
+        ),
+
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(40),
+          )
+        )
+
+
+
+
+      ),
+
