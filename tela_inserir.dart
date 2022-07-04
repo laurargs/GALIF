94821eca94745f615ace80f15e169d277d27a010
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-07-04 16:43:09 -0300

    Tirar Debug

--- tela_inserir.dart
+++ tela_inserir.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage( ),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData( ),
     );
   }
