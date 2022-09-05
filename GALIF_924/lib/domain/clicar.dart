4ad1108b12970acb05203a9d7da165ba4ee0049a
Josival <jcs28@aluno.ifal.edu.br>
2022-09-05 19:57:38 +0000

    implementacao_GridView

---
+++ GALIF_924/lib/domain/clicar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PacoteTuristico {
+  final String imagem;
+  final String onPressed;
+
+  PacoteTuristico({required this.imagem, required this.onPressed});
+}
