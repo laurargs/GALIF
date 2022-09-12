085ee8469a4b8e52bb934cac473837cb9f85d392
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-09-12 15:29:08 -0300

    Create notification.dart

---
+++ GALIF_924/lib/body/notification.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Notificacao extends StatefulWidget {
+  const Notificacao({Key? key}) : super(key: key);
+
+  @override
+  State<Notificacao> createState() => _NotificacaoState();
+  // List<String> avisos = ["cotelar ovos", "Seu dia de limpeza é amanhã!!","atualização do peso das galinha"];
+  // List<String> mensagem = ["o ninho 1 atingiu a menta de ovos para consumo", "remover bosta das galinha","Sua galinha afilhada aumentou 30% de massa"];
+  // //List<DateTime> hora = ["12:23",]
+  // List<String> hora = ["12:28","10;40","02:39"];
+
+}
+
+class _NotificacaoState extends State<Notificacao> {
+  List<String> avisos = ["cotelar ovos", "Seu dia de limpeza é amanhã!!","Atualização do peso das galinha"];
+  List<String> mensagem = ["o ninho 1 atingiu a menta de ovos para consumo", "remover bosta das galinha","Sua galinha afilhada aumentou 30% de massa"];
+  //List<DateTime> hora = ["12:23",]
+  List<String> hora = ["12:28","10:40","02:39"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        //SizedBox (width: 5),
+        //width: 0,
+        color: const Color(0xFFF25305),
+        child: ListView.builder(
+          itemCount: avisos.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              shape: Border(bottom: BorderSide(),),
+              leading: CircleAvatar(child: Text(avisos[index][0]),),
+              title: Text(avisos[index], 
+              style: TextStyle( fontSize: 16.0, fontWeight: FontWeight.bold, color: Color(0xFF571F04),),
+              ),
+              subtitle: Text(mensagem[index],
+              style: TextStyle( fontSize: 13.0, color: Color.fromARGB(255, 82, 50, 34),),
+              ),
+              trailing: Text(hora[index],
+              style: TextStyle( fontSize: 11.0, color: Color.fromARGB(255, 241, 149, 103),),
+              ),
+              //trailing: Text(hora[index] toString()),
+            );
+           }
+          ),
+       ),
+    );
+  }
+
+  void onPressedButton() {
+  }
+}
