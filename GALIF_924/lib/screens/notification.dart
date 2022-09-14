be46a4897fff2de90510dfc7a6a7a3666864facd
Kauan Nogueira <kna1@aluno.ifal.edu.br>
2022-09-14 17:10:03 +0000

    notificationup

--- GALIF_924/lib/screens/notification.dart
+++ GALIF_924/lib/screens/notification.dart
@@ -11,19 +11,25 @@ class _NotificacaoState extends State<Notificacao> {
   List<String> avisos = ["cotelar ovos", "Seu dia de limpeza é amanhã!!","Atualização do peso das galinha"];
   List<String> mensagem = ["o ninho 1 atingiu a menta de ovos para consumo", "remover bosta das galinha","Sua galinha afilhada aumentou 30% de massa"];
   List<String> hora = ["12:28","10:40","02:39"];
+  List<Icon> icons = [Icon(Icons.egg_rounded), Icon(Icons.cleaning_services_rounded), Icon(Icons.refresh_rounded)];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(16.0),
-        color: const Color(0xFFF25305),
+        color: const Color(0xFFF25305),  
         child: ListView.builder(
           itemCount: avisos.length,
           itemBuilder: (context, index){
-            return ListTile(
-              shape: Border(bottom: BorderSide()),
-              leading: CircleAvatar(child: Text(avisos[index][0]), backgroundColor: Color(0xFF571F04), foregroundColor: Colors.white,),
+            return Container(
+                decoration: BoxDecoration(
+                 border: Border(
+                   bottom: BorderSide(width: 0.8, color: Color(0xFF571F04)),
+                  ),
+                ),
+              child: ListTile(
+              leading: CircleAvatar(child: icons[index], backgroundColor: Color(0xFF571F04), foregroundColor: Colors.white,),
               title: Text(avisos[index], 
               style: TextStyle( fontSize: 16.0, fontWeight: FontWeight.bold, color: Color(0xFF571F04),),
               ),
@@ -33,13 +39,11 @@ class _NotificacaoState extends State<Notificacao> {
               trailing: Text(hora[index],
               style: TextStyle( fontSize: 11.0, color: Color.fromARGB(255, 241, 149, 103),),
               ),
-            );
+              ),
+              );
            }
           ),
        ),
-    );
-  }
-
-  void onPressedButton() {
+    ); 
   }
 }
