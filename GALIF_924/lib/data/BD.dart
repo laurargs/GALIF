7cb424e5e926fd9e7df49ab52c7b903019a833fa
Matheus-Ataide <matheusataide.al@gmail.com>
2022-09-28 20:13:11 +0000

    relocacao

--- GALIF_924/lib/data/BD.dart
+++ GALIF_924/lib/data/BD.dart
@@ -4,35 +4,35 @@ import 'package:flutter/cupertino.dart';
 class BD {
   static List<Individuos> lista = [
     Individuos(
-      imagem: 'assets/image/Galinha1.jpg',
+      imagem: 'assets/image/Galinhas/Galinha1.jpg',
       nome: 'Cócó',
       peso: '5kg',
       semanas: '10 semanas',
       especie: 'Galinha caipira',
     ),
     Individuos(
-      imagem: 'assets/image/Galinha2.jpg',
+      imagem: 'assets/image/Galinhas/Galinha2.jpg',
       nome: 'Teresa',
       peso: '2kg',
       semanas: '15 semanas',
       especie: 'Galinha caipira',
     ),
     Individuos(
-      imagem:'assets/image/Galinha3.jpg',
+      imagem:'assets/image/Galinhas/Galinha3.jpg',
       nome: 'Maria',
       peso: '4kg',
       semanas: '12 semanas',
       especie: 'Galinha do pé molhado',
     ),
     Individuos(
-      imagem: 'assets/image/Galinha4.jpeg',
+      imagem: 'assets/image/Galinhas/Galinha4.jpeg',
       nome: 'Josefina',
       peso: '7kg',
       semanas: '20 semanas',
       especie: 'Galinha do pé seco',
     ),
     Individuos(
-      imagem: 'assets/image/Galinha5.png',
+      imagem: 'assets/image/Galinhas/Galinha5.png',
       nome: 'Zeze',
       peso: '6kg',
       semanas: '14 semanas',
